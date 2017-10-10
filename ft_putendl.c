924ce1091a36f4e2eb56fdfc24991320ab56af71
gquesnot <gaqu1994@gmail.com>
2017-10-10 04:56:23 +0200

    ca

--- ft_putendl.c
+++ ft_putendl.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl.c                                       :+:      :+:    :+:   */
+/*   ft_putendl2.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gquesnot <gquesnot@student.le-101.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/08 00:57:42 by gquesnot          #+#    #+#             */
-/*   Updated: 2017/10/08 00:59:16 by gquesnot         ###   ########.fr       */
+/*   Created: 2017/10/10 03:15:24 by gquesnot          #+#    #+#             */
+/*   Updated: 2017/10/10 03:16:32 by gquesnot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_putendl(char const *s)
+void		ft_putendl(const char *s)
 {
 	ft_putstr(s);
 	ft_putchar('\n');
